219fcd161cf2d086459d372e1c982cec05aeabe6
Juanjo Cordero <juanjo@wearebrave.net>
2024-05-26 01:07:03 +0200

    http

--- src/app/gifs/pages/home/home-page.component.ts
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {GifsService} from "../../services/gifs.service";
 
 @Component({
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
+  
+  constructor( private gifsService: GifsService) {}
 
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Gif, SearchResponse} from "../interfaces/gifs.interfaces";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-
+  public gifsList:Gif[] = [];
   private _tagsHistory: string[] = [];
-  constructor() { }
+  private apiKey:string          = 'l5uEqwHlc3VKR3uOnp7KMcbQXUMbmklz';
+  private apiURL:string          = 'https://api.giphy.com/v1/gifs';
+  constructor( private http: HttpClient ) { }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -28,8 +32,20 @@ export class GifsService {
 
   searchTag( tag:string ):void{
     if ( tag.length === 0 ) return;
-
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key',this.apiKey)
+      .set('q',tag)
+      .set('limit','10')
+
+    this.http.get<SearchResponse>(`${ this.apiURL }/search`, {params})
+      .subscribe( resp => {
+        this.gifsList = resp.data;
+      });
+
+
+
   }
 
 }
